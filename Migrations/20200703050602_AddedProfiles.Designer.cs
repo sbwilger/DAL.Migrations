1ce730ed1be3861caab583a802140495885ae1b2
Serana Wilger <sbwilger@gmail.com>
2020-07-04 23:35:09 -0400

    Add files via upload

--- Migrations/20200703050602_AddedProfiles.Designer.cs
+++ Migrations/20200703050602_AddedProfiles.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sbwilger.DAL;
 
 namespace sbwilger.DAL.Migrations.Migrations
 {
     [DbContext(typeof(RPGContext))]
-    partial class RPGContextModelSnapshot : ModelSnapshot
+    [Migration("20200703050602_AddedProfiles")]
+    partial class AddedProfiles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
